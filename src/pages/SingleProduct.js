6de247d6a2b947354ef1f3f9cfbb02290b09975f
subhan300 <64373402+subhan300@users.noreply.github.com>
2021-02-20 03:32:55 +0500

    done

---
+++ src/pages/SingleProduct.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function SingleProduct() {
+    return (
+        <div>
+               <section>
+  <h2>Silver Stacking Ring</h2>
+  <p>$19.99</p>
+  <p>Wear one or seventeen! These rings are fun to mix and match.</p>
+  <button
+    class="snipcart-add-item"
+    data-item-id="silver-stacking-ring"
+    data-item-price="19.99"
+    data-item-url="https://gracious-sinoussi-df53b2.netlify.app/"
+    data-item-name="Silver Stacking Ring"
+    data-item-image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuaymWmmi-MUaYh89ZoboTsK1Gcu51QJ5OfA&usqp=CAU"
+  >
+    Add to cart
+  </button>
+</section>
+            
+        </div>
+    )
+}
+
+export default SingleProduct
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,22 +3,8 @@ import React from "react"
 export default function Home() {
   return(
     <>
-   <section>
-  <h2>Silver Stacking Ring</h2>
-  <p>$19.99</p>
-  <p>Wear one or seventeen! These rings are fun to mix and match.</p>
-  <button
-    class="snipcart-add-item"
-    data-item-id="silver-stacking-ring"
-    data-item-price="19.99"
-    data-item-url="https://gracious-sinoussi-df53b2.netlify.app/"
-    data-item-name="Silver Stacking Ring"
-    data-item-image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuaymWmmi-MUaYh89ZoboTsK1Gcu51QJ5OfA&usqp=CAU"
-  >
-    Add to cart
-  </button>
-</section>
 
+<h1>go to single product page</h1>
 
     </>
   )
